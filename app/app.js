81129aab1a8b5ba74a782643d33c898ac9ec079a
Yevhen Khabarov <yevhen.khabarov@uvoteam.com>
2020-01-04 22:28:24 +0200

    s17.304 add multiple routes

--- app/app.js
+++ app/app.js
@@ -9,8 +9,20 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('HELLO FROM OUR WEB APP\n');
+  res.send('HELLO FROM OUR WEB APP!\n');
 });
+
+app.get('/joke', (req, res) => {
+  let joke = "joke test";
+  res.send(joke);
+  console.log('joke page');
+});
+
+app.get('/random_num', (req, res) => {
+  let num = Math.floor(Math.random() * 10) + 1;
+  res.send(`Your lucky number is ${num}`);
+});
+
 
 app.listen(PORT, HOST, ()=> {
   console.log(`App running on http://${HOST}:${PORT}`)
